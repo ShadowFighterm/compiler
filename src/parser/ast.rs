4203a5ab600df7dbaef91ef043b2fd540e0c1089
smirfanshah <irfanshah0422@gmail.com>
2025-09-21 11:37:27 +0500

    feat: AST for Expr, Stmt, Parameters, Program and declarations

---
+++ src/parser/ast.rs
@@ -0,0 +1,82 @@
+use crate::token::TokenKind;
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Identifier(String),
+    Integer(i64),
+    Float(f64),
+    Boolean(bool),
+    StringLit(String),
+    Binary {
+        left: Box<Expr>,
+        operator: TokenKind,
+        right: Box<Expr>,
+    },
+    Unary {
+        operator: TokenKind,
+        expr: Box<Expr>,
+    },
+    Call {
+        callee: Box<Expr>,
+        args: Vec<Expr>,
+    },
+    Grouping(Box<Expr>),
+}
+
+#[derive(Debug, Clone)]
+pub enum Stmt {
+    Expr(Expr),
+    Let {
+        name: String,
+        type_annot: Option<TokenKind>, // T_INT, T_FLOAT, etc.
+        value: Expr,
+    },
+    Block(Vec<Stmt>),
+    Return(Option<Expr>),
+    If {
+        condition: Expr,
+        then_branch: Box<Stmt>,
+        else_branch: Option<Box<Stmt>>,
+    },
+    While {
+        condition: Expr,
+        body: Box<Stmt>,
+    },
+    For {
+        init: Option<Box<Stmt>>,
+        condition: Option<Expr>,
+        increment: Option<Expr>,
+        body: Box<Stmt>,
+    },
+    Function {
+        name: String,
+        params: Vec<Param>,
+        return_type: Option<TokenKind>,
+        body: Box<Stmt>,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub struct Param {
+    pub name: String,
+    pub param_type: TokenKind, // T_INT, T_FLOAT, etc.
+}
+
+#[derive(Debug, Clone)]
+pub enum Decl {
+    Function {
+        name: String,
+        params: Vec<Param>,
+        return_type: Option<TokenKind>,
+        body: Box<Stmt>,
+    },
+    GlobalVar {
+        name: String,
+        type_annot: Option<TokenKind>,
+        value: Option<Expr>,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub struct Program {
+    pub declarations: Vec<Decl>,
+}
