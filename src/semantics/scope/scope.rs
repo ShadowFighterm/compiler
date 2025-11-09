eb3059935673c9fb46fade36c6a0ee26c1fc07a2
smirfanshah <irfanshah0422@gmail.com>
2025-11-09 23:39:07 +0500

    feat: basic ScopeStack heirarchy i.e push, pop, insert, lookup

--- src/semantics/scope/scope.rs
+++ src/semantics/scope/scope.rs
@@ -19,6 +19,7 @@ pub enum symbolKind{
 pub struct Symbol{
     pub name: String,
     pub kind: symbolKind,
+    pub scope_level: usize,
     pub data_type: Option<String>,
     pub value: Option<String>,
 }
@@ -45,7 +46,41 @@ impl ScopeStack{
         ScopeStack{ current: None }
     }
     
-    // pub fn push_scope(&mut self){
+    pub fn push_scope(&mut self){
+        let new_scope = Scope::new(self.current.take());
+        self.current = Some(Box::new(new_scope)) 
+    }
 
-    // }
+    pub fn pop_scope(&mut self){
+        if let Some(cur) = self.current.take(){
+            self.current = cur.parent;
+        }
+    }
+
+    pub fn insert_symbol(&mut self, name: String, symbol: Symbol,) -> Result<(), ScopeError>
+    {
+        if let Some(curr) = self.current.as_mut() {
+            if curr.symbols.contains_key(&name) {
+                return Err(ScopeError::VariableRedefinition);
+            }
+            curr.symbols.insert(name, symbol);
+            Ok(())
+        } else {
+            // No scope exists yet, create global
+            self.push_scope();
+            self.insert_symbol(name, symbol)
+        }
+    }
+    pub fn lookup_symbol(&self, name: &str) -> Result<Symbol, ScopeError> {
+        let mut scope_opt = self.current.as_ref();
+        while let Some(scope) = scope_opt {
+            if let Some(sym) = scope.symbols.get(name) {
+                return Ok(sym.clone());
+            }
+            scope_opt = scope.parent.as_ref();
+        }
+        Err(ScopeError::UndeclaredVariableAccessed)
+    }
 }
+
+
