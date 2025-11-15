a27dcce0c0ebca3ec620c39ad26a13b74dca8084
smirfanshah <irfanshah0422@gmail.com>
2025-11-16 04:37:55 +0500

    feat: add type check enum

---
+++ src/semantics/typechecker.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+pub enum TypeChkError {
+    ErroneousVarDecl,
+    FnCallParamCount,
+    FnCallParamType,
+    ErroneousReturnType,
+    ExpressionTypeMismatch,
+    ExpectedBooleanExpression,
+    ErroneousBreak,
+    NonBooleanCondStmt,
+    EmptyExpression,
+    AttemptedBoolOpOnNonBools,
+    AttemptedBitOpOnNonNumeric,
+    AttemptedShiftOnNonInt,
+    AttemptedAddOpOnNonNumeric,
+    AttemptedExponentiationOfNonNumeric,
+    ReturnStmtNotFound,
+}
